b56cc707faf38f4fd6db7edee32a66ddb81507c8
Keegan Mullaney <kmullaney@newrelic.com>
2023-04-21 11:14:14 -0700

    add cron jobs

---
+++ cpm-cron-job.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# -----------------------------------------------------
+# CPM Cron Job
+# Helps to keep docker clean and the CPM updated.
+# Can also be used to start the CPM.
+#
+# Author : Keegan Mullaney
+# Company: New Relic
+# Email  : kmullaney@newrelic.com
+# Website: github.com/keegoid-nr/useful-scripts
+# License: MIT
+# -----------------------------------------------------
+
+# run as root if the docker group has not yet been added to your user
+# https://docs.docker.com/engine/install/linux-postinstall/
+# if [[ $EUID -ne 0 ]]; then
+#   echo "This script must be run as root"
+#   exit 1
+# fi
+
+# A recursive function to stop all containers and prune containers, images, and networks not in use until no docker containers exist.
+function stop_and_prune_containers {
+  # stop all containers (assuming this host is dedicated to the CPM)
+  docker stop $(docker ps -aq) 2>/dev/null
+
+  # prune containers, images, and networks not in use
+  docker system prune -af
+
+  # check if any containers still exist
+  if [ "$(docker ps -aq)" ]; then
+    stop_and_prune_containers # recursively call function until no containers exist
+  fi
+}
+
+# stop and prune all containers until none exist
+stop_and_prune_containers
+
+# start new job manager to support monitoring activities
+# avoid using sudo with the docker run command since containers spawned by the minion won't inherit elevated permissions
+docker run --name=YOUR_MINION_NAME -e MINION_PRIVATE_LOCATION_KEY=YOUR_PRIVATE_LOCATION_KEY -d --restart unless-stopped -v /tmp:/tmp:rw -v /var/run/docker.sock:/var/run/docker.sock:rw quay.io/newrelic/synthetics-minion:latest
---
+++ sjm-cron-job.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# -----------------------------------------------------
+# SJM Cron Job
+# Helps to keep docker clean and the SJM updated.
+# Can also be used to start the SJM.
+#
+# Author : Keegan Mullaney
+# Company: New Relic
+# Email  : kmullaney@newrelic.com
+# Website: github.com/keegoid-nr/useful-scripts
+# License: MIT
+# -----------------------------------------------------
+
+# run as root if the docker group has not yet been added to your user
+# https://docs.docker.com/engine/install/linux-postinstall/
+# if [[ $EUID -ne 0 ]]; then
+#   echo "This script must be run as root"
+#   exit 1
+# fi
+
+# A recursive function to stop all containers and prune containers, images, and networks not in use until no docker containers exist.
+function stop_and_prune_containers {
+  # stop all containers (assuming this host is dedicated to the SJM)
+  docker stop $(docker ps -aq) 2>/dev/null
+
+  # prune containers, images, and networks not in use
+  docker system prune -af
+
+  # check if any containers still exist
+  if [ "$(docker ps -aq)" ]; then
+    stop_and_prune_containers # recursively call function until no containers exist
+  fi
+}
+
+# stop and prune all containers until none exist
+stop_and_prune_containers
+
+# start new job manager to support monitoring activities
+# avoid using sudo with the docker run command since containers spawned by the job manager won't inherit elevated permissions
+docker run --name YOUR_CONTAINER_NAME -e PRIVATE_LOCATION_KEY=YOUR_PRIVATE_LOCATION_KEY -d --restart unless-stopped -v /var/run/docker.sock:/var/run/docker.sock:rw newrelic/synthetics-job-manager:latest
