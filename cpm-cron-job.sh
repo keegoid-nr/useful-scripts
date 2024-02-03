93292dd56f07cef43f0fd9b59f8d965e86c31e00
Keegan Mullaney <kmullaney@newrelic.com>
2024-02-02 18:18:16 -0800

    feat: otlp and cpm update

--- cpm-cron-job.sh
+++ cpm-cron-job.sh
@@ -14,13 +14,20 @@
 # A recursive function to stop all containers and prune containers, images, and networks not in use until no docker containers exist.
 function stop_and_prune_containers {
   # stop all containers (assuming this host is dedicated to the CPM)
-  docker stop $(docker ps -aq) 2>/dev/null
+  # docker stop $(docker ps -aq) 2>/dev/null
+
+  # stop only CPM-related containers
+  docker stop $(docker ps -qf 'label=name=synthetics-minion') 2>/dev/null
+  docker stop $(docker ps -qf 'label=name=synthetics-minion-runner') 2>/dev/null
 
   # prune containers, images, and networks not in use
   docker system prune -af
 
   # check if any containers still exist
-  if [ "$(docker ps -aq)" ]; then
+  # if [ "$(docker ps -aq)" ]; then
+
+  # check if CPM-related containers still exist
+  if [ "$(docker ps -qf 'label=name=synthetics-minion')" ] || [ "$(docker ps -qf 'label=name=synthetics-minion-runner')" ]; then
     stop_and_prune_containers # recursively call function until no containers exist
   fi
 }
@@ -28,6 +35,6 @@ function stop_and_prune_containers {
 # stop and prune all containers until none exist
 stop_and_prune_containers
 
-# start new job manager to support monitoring activities
+# start a new minion to support monitoring activities
 # avoid using sudo with the docker run command since containers spawned by the minion won't inherit elevated permissions
 docker run --name YOUR_CONTAINER_NAME -e MINION_PRIVATE_LOCATION_KEY=YOUR_PRIVATE_LOCATION_KEY -v /tmp:/tmp:rw -v /var/run/docker.sock:/var/run/docker.sock:rw -d --restart unless-stopped quay.io/newrelic/synthetics-minion:latest
---
+++ test-otlp-endpoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the target domain for mtr
+TARGET_DOMAIN="otlp.nr-data.net"
+TARGET_PORT="4318"
+PAGE="v1/metrics"
+URL="https://$TARGET_DOMAIN:$TARGET_PORT/$PAGE"
+
+# Send a POST request to the specified URL with the specified headers
+# The -o /dev/null discards the body of the response, keeping just the HTTP status code
+# The -w option specifies a custom output format to include the IP address and HTTP status code
+OTLP_INFO=$(curl -X POST -o /dev/null -s -w "IP: %{remote_ip}, Response Code: %{http_code}\n" -H "Content-Type: application/json" -H "api-key: $NEW_RELIC_LICENSE_KEY" $URL)
+
+# Print the IP address and response code for the OTLP request
+echo "$OTLP_INFO"
