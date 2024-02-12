c92821536ca8d7c5878ccc1dc18f5fd61126ca11
Keegan Mullaney <kmullaney@newrelic.com>
2024-02-12 14:20:43 -0800

    feat: loop cnt and sjm specific ccommands

--- cpm-cron-job.sh
+++ cpm-cron-job.sh
@@ -13,6 +13,9 @@
 
 # A recursive function to stop all containers and prune containers, images, and networks not in use until no docker containers exist.
 function stop_and_prune_containers {
+  local cnt=${1:-1} # Initialize counter or get it as an argument
+  echo "recursive loop: $cnt"
+
   # stop all containers (assuming this host is dedicated to the CPM)
   # docker stop $(docker ps -aq) 2>/dev/null
 
@@ -28,7 +31,7 @@ function stop_and_prune_containers {
 
   # check if CPM-related containers still exist
   if [ "$(docker ps -qf 'label=name=synthetics-minion')" ] || [ "$(docker ps -qf 'label=name=synthetics-minion-runner')" ]; then
-    stop_and_prune_containers # recursively call function until no containers exist
+    stop_and_prune_containers $((cnt+1)) # recursively call function with incremented counter until no CPM containers exist
   fi
 }
 
@@ -38,5 +41,5 @@ stop_and_prune_containers
 # start a new minion to support monitoring activities
 # avoid using sudo with the docker run command since containers spawned by the minion won't inherit elevated permissions
 # the log-opt tag will make it easier to find container logs if forwarding to New Relic
-# 8080 and 8180 expose admin endpoints like :8080/status/check and :8180/healthcheck?pretty=true
+# 8080 and 8180 expose admin endpoints like http://localhost:8080/status/check and http://localhost:8180/healthcheck?pretty=true
 docker run --name YOUR_CONTAINER_NAME -e MINION_PRIVATE_LOCATION_KEY=YOUR_PRIVATE_LOCATION_KEY -v /tmp:/tmp:rw -v /var/run/docker.sock:/var/run/docker.sock:rw -p 8080:8080 -p 8180:8180 -d --restart unless-stopped --log-opt tag="{{.Name}}/{{.ID}}" quay.io/newrelic/synthetics-minion:latest | tee -a docker-run.log 2>&1
--- sjm-cron-job.sh
+++ sjm-cron-job.sh
@@ -13,15 +13,29 @@
 
 # A recursive function to stop all containers and prune containers, images, and networks not in use until no docker containers exist.
 function stop_and_prune_containers {
+  local cnt=${1:-1} # Initialize counter or get it as an argument
+  echo "recursive loop: $cnt"
+
   # stop all containers (assuming this host is dedicated to the SJM)
-  docker stop $(docker ps -aq) 2>/dev/null
+  # docker stop $(docker ps -aq) 2>/dev/null
+
+  # stop only SJM-related containers
+  docker stop $(docker ps -qf 'label=application=synthetics-job-manager') 2>/dev/null
+  docker stop $(docker ps -qf 'label=application=synthetics-ping-runtime') 2>/dev/null
+  docker stop $(docker ps -qf 'ancestor=newrelic/synthetics-node-api-runtime') 2>/dev/null
+  docker stop $(docker ps -qf 'ancestor=newrelic/synthetics-node-browser-runtime') 2>/dev/null
 
   # prune containers, images, and networks not in use
   docker system prune -af
 
-  # check if any containers still exist
-  if [ "$(docker ps -aq)" ]; then
-    stop_and_prune_containers # recursively call function until no containers exist
+  # # check if any containers still exist
+  # if [ "$(docker ps -aq)" ]; then
+  #   stop_and_prune_containers # recursively call function until no containers exist
+  # fi
+
+  # check if SJM-related containers still exist
+  if [ "$(docker ps -qf 'label=application=synthetics-job-manager')" ] || [ "$(docker ps -qf 'label=application=synthetics-ping-runtime')" ] || [ "$(docker ps -qf 'ancestor=newrelic/synthetics-node-api-runtime')" ] || [ "$(docker ps -qf 'ancestor=newrelic/synthetics-node-browser-runtime')" ]; then
+    stop_and_prune_containers $((cnt+1)) # recursively call function with incremented counter until no SJM containers exist
   fi
 }
 
@@ -31,5 +45,5 @@ stop_and_prune_containers
 # start new job manager to support monitoring activities
 # avoid using sudo with the docker run command since containers spawned by the job manager won't inherit elevated permissions
 # the log-opt tag will make it easier to find container logs if forwarding to New Relic
-# ports 8080 and 8082 expose admin endpoints like :8080/status/check and :8082/healthcheck?pretty=true
+# ports 8080 and 8082 expose admin endpoints like http://localhost:8080/status/check and http://localhost:8082/healthcheck?pretty=true
 docker run --name YOUR_CONTAINER_NAME -e PRIVATE_LOCATION_KEY=YOUR_PRIVATE_LOCATION_KEY -v /var/run/docker.sock:/var/run/docker.sock:rw -p 8080:8080 -p 8082:8082 -d --restart unless-stopped --log-opt tag="{{.Name}}/{{.ID}}" newrelic/synthetics-job-manager:latest | tee -a docker-run.log 2>&1
