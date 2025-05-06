6a5966ee3447f2d3bdfdd61538741598274c2774
Keegan Mullaney <kmullaney@newrelic.com>
2025-05-05 18:34:03 -0700

    add HOST_IP_ADDRESS for podman script

--- sjm-cron-job-podman.sh
+++ sjm-cron-job-podman.sh
@@ -17,13 +17,15 @@
 # Default values for global variables
 DEFAULT_SJM_CONTAINER_NAME="YOUR_SJM_CONTAINER_NAME"
 DEFAULT_SJM_POD_NAME="YOUR_SJM_POD_NAME"
+DEFAULT_HOST_IP_ADDRESS="YOUR_HOST_IP_ADDRESS"
 DEFAULT_SJM_PRIVATE_LOCATION_KEY="YOUR_SJM_PRIVATE_LOCATION_KEY"
 SJM_IMAGE="docker.io/newrelic/synthetics-job-manager:latest"
 
 # Use provided command-line arguments or fallback to default values
 SJM_CONTAINER_NAME="${1:-$DEFAULT_SJM_CONTAINER_NAME}"
 SJM_POD_NAME="${2:-$DEFAULT_SJM_POD_NAME}"
-SJM_PRIVATE_LOCATION_KEY="${3:-$DEFAULT_SJM_PRIVATE_LOCATION_KEY}"
+HOST_IP_ADDRESS="${3:-$DEFAULT_HOST_IP_ADDRESS}"
+SJM_PRIVATE_LOCATION_KEY="${4:-$DEFAULT_SJM_PRIVATE_LOCATION_KEY}"
 
 # Function to stop and prune containers and pods
 function stop_and_prune_containers {
@@ -40,7 +42,9 @@ function stop_and_prune_containers {
 stop_and_prune_containers
 
 # Create a new pod with port mappings and host entry for Podman API
-podman pod create --network slirp4netns --name "$SJM_POD_NAME" --add-host=podman.service:$(hostname -i) -p 8080:8080 -p 8082:8082
+# Replace HOST_IP_ADDRESS with the actual IP address of your host
+# If using WSL2 in Windows, access the Linux distribution `wsl -d Ubuntu-24.04` then find the IP address with `ip a s eth0 | grep inet`
+podman pod create --network slirp4netns --name "$SJM_POD_NAME" --add-host=podman.service:"$HOST_IP_ADDRESS" -p 8080:8080 -p 8082:8082
 
 # Start new job manager to support monitoring activities
 podman run \
